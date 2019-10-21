203c6c0be21e17035691f9645784026183a6d2bd
Chelsea Cui <acui@andrew.cmu.edu>
2019-10-20 22:08:44 -0400

    add serializer

---
+++ api/app/serializers/station_serializer.rb
@@ -0,0 +1,5 @@
+# class StationSerializer < ActiveModel::Serializer
+#   attributes :name, :longitude, :latitude, :introduction
+#   has_many :memes
+# end
+# TODO: do we need serialization? if yes, how to deal with filter?
