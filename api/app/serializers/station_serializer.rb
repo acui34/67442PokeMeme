6fe6953cfbd59641c2ad71a229d2bb18c479440c
Chelsea Cui <acui@andrew.cmu.edu>
2019-10-20 22:19:23 -0400

    add versionizing

--- api/app/serializers/station_serializer.rb
+++
@@ -1,5 +0,0 @@
-# class StationSerializer < ActiveModel::Serializer
-#   attributes :name, :longitude, :latitude, :introduction
-#   has_many :memes
-# end
-# TODO: do we need serialization? if yes, how to deal with filter?
--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users
-  resources :stations
-  resources :memes
+
+  scope module: 'api' do
+    namespace :v1 do
+      resources :users
+      resources :stations
+      resources :memes
+    end
+  end
 end
